666c5dab09a0ea72087cdcbd783648c3fbd880ce
hienvanla5 <hienvla05@gmail.com>
2026-07-14 10:06:58 +0700

    create basic file security

---
+++ backend/src/main/java/vn/nguongocso/controller/AdminController.java
@@ -0,0 +1,15 @@
+package vn.nguongocso.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+public class AdminController {
+
+    @GetMapping
+    public String adminPage() {
+        return "This is admin page";
+    }
+}
---
+++ backend/src/main/java/vn/nguongocso/controller/HelloController.java
@@ -0,0 +1,15 @@
+package vn.nguongocso.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/public")
+public class HelloController {
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        return "Hello World!";
+    }
+}
