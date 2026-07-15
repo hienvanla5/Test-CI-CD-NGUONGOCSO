8ee15ccc6fd4a1613678cbb4841cec93d6fcb982
hienvanla5 <hienvla05@gmail.com>
2026-07-15 08:02:39 +0700

    update organization type

--- backend/src/main/java/vn/nguongocso/auth/enums/OrganizationType.java
+++ backend/src/main/java/vn/nguongocso/auth/enums/OrganizationType.java
@@ -2,7 +2,7 @@ package vn.nguongocso.auth.enums;
 
 
 public enum OrganizationType {
-    COOPERATIVE,    // Hợp tác xã
-    ENTERPRISE,     // Doanh nghiệp
-    GOVERNMENT      // Cơ quan quản lý
+    COOPERATIVE,
+    ENTERPRISE,
+    GOVERNMENT
 }
