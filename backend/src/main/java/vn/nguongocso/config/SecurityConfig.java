1dab470cb136c8cfdb1b118b7c0c18091585fc22
hienvanla5 <hienvla05@gmail.com>
2026-07-18 09:59:20 +0700

    add cors port 5500

--- backend/src/main/java/vn/nguongocso/config/SecurityConfig.java
+++ backend/src/main/java/vn/nguongocso/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "http://localhost:63342/"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "http://localhost:63342/", "http://localhost:5500/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setExposedHeaders(List.of("Authorization"));
