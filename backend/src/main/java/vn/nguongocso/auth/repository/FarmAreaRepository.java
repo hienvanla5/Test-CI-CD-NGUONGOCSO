9295e069736b00386b1e3abbb0e91b342d95c614
laodai2035-boop <laodai12035@gmail.com>
2026-07-20 10:41:07 +0700

    Xoa cac file con lai o auth

--- backend/src/main/java/vn/nguongocso/auth/repository/FarmAreaRepository.java
+++
@@ -1,9 +0,0 @@
-package vn.nguongocso.auth.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import vn.nguongocso.auth.entity.FarmArea;
-import java.util.UUID;
-
-public interface FarmAreaRepository extends JpaRepository<FarmArea, UUID> {
-}
-
