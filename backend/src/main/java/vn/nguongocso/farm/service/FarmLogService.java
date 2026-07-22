babcd15248d382d1a63bd6ce1a3c2690770fb851
laodai2035-boop <laodai12035@gmail.com>
2026-07-22 15:47:57 +0700

    feat: Bo sung phuong thuc getLogsByProductionLot vao FarmLogService

--- backend/src/main/java/vn/nguongocso/farm/service/FarmLogService.java
+++ backend/src/main/java/vn/nguongocso/farm/service/FarmLogService.java
@@ -1,9 +1,13 @@
 package vn.nguongocso.farm.service;
 
+import java.util.List;
+import java.util.UUID;
 import vn.nguongocso.farm.dto.request.CreateFarmLogRequest;
 import vn.nguongocso.farm.dto.response.FarmLogResponse;
 
 public interface FarmLogService {
 
 	FarmLogResponse create(CreateFarmLogRequest request);
+
+	List<FarmLogResponse> getLogsByProductionLot(UUID productionLotId);
 }
