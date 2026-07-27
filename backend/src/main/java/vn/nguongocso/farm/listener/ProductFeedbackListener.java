f494cd180f3d6e43fe1060c812d47591f3f96440
laodai2035-boop <laodai12035@gmail.com>
2026-07-27 10:30:59 +0700

    feat: Khai báo Listener nhận log thông báo [farm/listener]

---
+++ backend/src/main/java/vn/nguongocso/farm/listener/ProductFeedbackListener.java
@@ -0,0 +1,23 @@
+package vn.nguongocso.farm.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import vn.nguongocso.farm.event.ProductFeedbackSubmittedEvent;
+
+@Component
+public class ProductFeedbackListener {
+    private static final Logger log = LoggerFactory.getLogger(ProductFeedbackListener.class);
+
+    @Async
+    @EventListener
+    public void handleProductFeedbackSubmitted(ProductFeedbackSubmittedEvent event) {
+        log.warn("CẢNH BÁO PHẢN ÁNH SẢN PHẨM: Nhận được phản ánh mới về Lô sản xuất '{}' (ID: {}), thuộc tổ chức có ID: {}. Nội dung phản ánh: \"{}\"",
+                event.getProductionLotName(),
+                event.getProductionLotId(),
+                event.getOrganizationId(),
+                event.getContent());
+    }
+}
