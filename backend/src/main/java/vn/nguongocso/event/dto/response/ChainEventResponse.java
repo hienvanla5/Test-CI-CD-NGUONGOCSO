97cbc530f5ab0e70bc1ee0383287ef08d2fe7251
tpd05 <doantran28092005@gmail.com>
2026-07-27 10:07:21 +0700

    feat: record-transport-event

--- backend/src/main/java/vn/nguongocso/event/dto/response/ChainEventResponse.java
+++ backend/src/main/java/vn/nguongocso/event/dto/response/ChainEventResponse.java
@@ -10,11 +10,14 @@ import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 /**
  * DTO phản hồi chuỗi sự kiện cung ứng.
  *
  * @author Team WEB 1
  */
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Getter
 @Setter
 @Builder
